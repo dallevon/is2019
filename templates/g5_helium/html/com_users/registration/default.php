5be4e80d2e1a204e50973b41120ecf03863296b2
Levon <dallevon@gmail.com>
2019-03-02 16:57:24 -0500

    template: override joomla com_user

--- templates/g5_helium/html/com_users/registration/default.php
+++ templates/g5_helium/html/com_users/registration/default.php
@@ -12,6 +12,53 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidator');
 
+/*** Begin Registration Form Override ***/
+
+$doc = JFactory::getDocument();
+$js = "
+	jQuery(document).ready(function($){
+		// Define the variables
+		const regForm = $('#member-registration');
+		const username = regForm.find('#jform_username');
+		// const name = regForm.find('#jform_name');
+		const password = regForm.find('#jform_password1');
+		const password2 = regForm.find('#jform_password2');
+		const email = regForm.find('#jform_email1');
+		const email2 = regForm.find('#jform_email2');
+		 
+		
+		// Hide the required field, star, username, name, confirm pass and confirm email and exclude from validation
+		regForm.find('.spacer').parents('.control-group').hide();
+		regForm.find('.star').hide();
+		username.parents('.control-group').hide();
+		username.addClass('novalidate');
+		// name.parents('.control-group').hide();
+		// name.addClass('novalidate');
+		password2.parents('.control-group').hide();
+		password2.addClass('novalidate');
+		email2.parents('.control-group').hide();
+		email2.addClass('novalidate');
+		
+		(email.parents('.control-group')).after(password.parents('.control-group'));
+		
+		// Add a default value to the name field
+		// name.val('IS2109_DEFAULT_USER_NAME');
+
+		
+		// Clone password and email values to the confirm fields and username
+		email.on('keyup', function() {
+				username.val( this.value );
+				email2.val( this.value );
+		});
+		password.on('keyup', function() {
+				password2.val( this.value );
+		});
+	});    
+";
+$doc->addScriptDeclaration($js);
+
+/*** Finish Registration Form Override ***/
+
 ?>
 <div class="registration<?php echo $this->pageclass_sfx; ?>">
 	<?php if ($this->params->get('show_page_heading')) : ?>
@@ -19,7 +66,7 @@ JHtml::_('behavior.formvalidator');
 			<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
 		</div>
 	<?php endif; ?>
-	<form id="member-registration" action="<?php echo JRoute::_('index.php?option=com_users&task=registration.register'); ?>" method="post" class="form-validate form-horizontal well" enctype="multipart/form-data">
+	<form id="member-registration" action="<?php echo JRoute::_('index.php?option=com_users&task=registration.register'); ?>" method="post" class="form-validate well" enctype="multipart/form-data">
 		<?php // Iterate through the form fieldsets and display each one. ?>
 		<?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
 			<?php $fields = $this->form->getFieldset($fieldset->name); ?>
