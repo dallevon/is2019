0918af05cade175a15669fd4efc7807584e2cff1
Levon <dallevon@gmail.com>
2019-03-17 10:44:31 -0400

    overrides: addtocart button

--- templates/g5_helium/html/com_virtuemart/sublayouts/addtocartbtn.php
+++ templates/g5_helium/html/com_virtuemart/sublayouts/addtocartbtn.php
@@ -17,5 +17,5 @@ defined('_JEXEC') or die('Restricted access');
 if ($viewData['orderable']) {
 	echo '<input type="submit" name="addtocart" class="addtocart-button button button-bevel button-xsmall" value="' . vmText::_('COM_VIRTUEMART_CART_ADD_TO') . '" title="' . vmText::_('COM_VIRTUEMART_CART_ADD_TO') . '" />';
 } else {
-	// echo '<span name="addtocart" class="addtocart-button-disabled" title="'.vmText::_( 'COM_VIRTUEMART_ADDTOCART_CHOOSE_VARIANT' ).'" >'.vmText::_( 'COM_VIRTUEMART_ADDTOCART_CHOOSE_VARIANT' ).'</span>';
+	echo '<span name="addtocart" class="addtocart-button-disabled" title="' . vmText::_('COM_VIRTUEMART_ADDTOCART_EXPLANATION') . '" >' . vmText::_('COM_VIRTUEMART_EXPLANATION') . '</span>';
 }
