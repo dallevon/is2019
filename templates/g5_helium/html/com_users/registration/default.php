6e371954b038043ed3f3c10dc6a406706510f3c0
Levon <dallevon@gmail.com>
2019-03-17 10:35:15 -0400

    overrides: com_user registration

--- templates/g5_helium/html/com_users/registration/default.php
+++ templates/g5_helium/html/com_users/registration/default.php
@@ -61,37 +61,39 @@ $doc->addScriptDeclaration($js);
 
 ?>
 <div class="registration<?php echo $this->pageclass_sfx; ?>">
-	<?php if ($this->params->get('show_page_heading')) : ?>
-		<div class="page-header">
-			<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
-		</div>
-	<?php endif; ?>
-	<form id="member-registration" action="<?php echo JRoute::_('index.php?option=com_users&task=registration.register'); ?>" method="post" class="form-validate well" enctype="multipart/form-data">
-		<?php // Iterate through the form fieldsets and display each one. ?>
-		<?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
-			<?php $fields = $this->form->getFieldset($fieldset->name); ?>
-			<?php if (count($fields)) : ?>
-				<fieldset>
-					<?php // If the fieldset has a label set, display it as the legend. ?>
-					<?php if (isset($fieldset->label)) : ?>
-						<legend><?php echo JText::_($fieldset->label); ?></legend>
-					<?php endif; ?>
-					<?php echo $this->form->renderFieldset($fieldset->name); ?>
-				</fieldset>
-			<?php endif; ?>
-		<?php endforeach; ?>
-		<div class="control-group">
-			<div class="controls">
-				<button type="submit" class="btn btn-primary validate">
-					<?php echo JText::_('JREGISTER'); ?>
-				</button>
-				<a class="btn" href="<?php echo JRoute::_(''); ?>" title="<?php echo JText::_('JCANCEL'); ?>">
-					<?php echo JText::_('JCANCEL'); ?>
-				</a>
-				<input type="hidden" name="option" value="com_users" />
-				<input type="hidden" name="task" value="registration.register" />
-			</div>
-		</div>
-		<?php echo JHtml::_('form.token'); ?>
-	</form>
+  <?php if ($this->params->get('show_page_heading')) : ?>
+  <div class="page-header">
+    <h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
+  </div>
+  <?php endif; ?>
+  <form id="member-registration"
+    action="<?php echo JRoute::_('index.php?option=com_users&task=registration.register'); ?>" method="post"
+    class="form-validate well" enctype="multipart/form-data">
+    <?php  ?>
+    <?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
+    <?php $fields = $this->form->getFieldset($fieldset->name); ?>
+    <?php if (count($fields)) : ?>
+    <fieldset>
+      <?php  ?>
+      <?php if (isset($fieldset->label)) : ?>
+      <legend><?php echo JText::_($fieldset->label); ?></legend>
+      <?php endif; ?>
+      <?php echo $this->form->renderFieldset($fieldset->name); ?>
+    </fieldset>
+    <?php endif; ?>
+    <?php endforeach; ?>
+    <div class="control-group">
+      <div class="controls">
+        <button type="submit" class="button validate">
+          <?php echo JText::_('JREGISTER'); ?>
+        </button>
+        <a class="button button-outline" href="<?php echo JRoute::_(''); ?>" title="<?php echo JText::_('JCANCEL'); ?>">
+          <?php echo JText::_('JCANCEL'); ?>
+        </a>
+        <input type="hidden" name="option" value="com_users" />
+        <input type="hidden" name="task" value="registration.register" />
+      </div>
+    </div>
+    <?php echo JHtml::_('form.token'); ?>
+  </form>
 </div>
