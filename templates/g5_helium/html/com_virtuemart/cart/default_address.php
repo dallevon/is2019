441def79c792e18066b786352fcbf24e431540b7
Levon <dallevon@gmail.com>
2019-04-15 19:17:32 -0400

    override: cart address presentation

--- templates/g5_helium/html/com_virtuemart/cart/default_address.php
+++ templates/g5_helium/html/com_virtuemart/cart/default_address.php
@@ -26,23 +26,22 @@ echo '<div class="billto-shipto">';
 
 echo '<div class="output-billto">';
 echo '<h2><span class="fa fa-home fw"></span>&nbsp;' . vmText::_('COM_VIRTUEMART_USER_FORM_BILLTO_LBL') . '</h2>';
-echo '<div>';
 $cartfieldNames = array();
 foreach ($this->userFieldsCart['fields'] as $fields) {
   $cartfieldNames[] = $fields['name'];
 }
-
 foreach ($this->cart->BTaddress['fields'] as $item) {
   if (in_array($item['name'], $cartfieldNames)) continue;
   if (!empty($item['value'])) {
     if ($item['name'] === 'agreed') {
       $item['value'] = ($item['value'] === 0) ? vmText::_('COM_VIRTUEMART_USER_FORM_BILLTO_TOS_NO') : vmText::_('COM_VIRTUEMART_USER_FORM_BILLTO_TOS_YES');
     }
-    // echo '<span class="titles">' . $item['title'] . '</span>';
+    echo '<div class="field">';
+    echo '<span class="titles">' . $item['title'] . '</span>';
     echo '<span class="values vm2-' . $item['name'] . '">' . $item['value'] . '</span>';
+    echo '</div>';
   }
 }
-echo '</div>';
 
 if ($this->pointAddress) {
   $this->pointAddress = 'required invalid';
@@ -56,9 +55,8 @@ echo '</div>';
 
 echo '<div class="output-shipto">';
 echo '<h2><span class="fa fa-envelope fw"></span>&nbsp;' . vmText::_('COM_VIRTUEMART_USER_FORM_SHIPTO_LBL') . '</h2>';
-echo  '<div>';
-if ($this->cart->user->virtuemart_user_id == 0) {
 
+if ($this->cart->user->virtuemart_user_id == 0) {
   echo vmText::_('COM_VIRTUEMART_USER_FORM_ST_SAME_AS_BT');
   echo VmHtml::checkbox('STsameAsBT', $this->cart->STsameAsBT, 1, 0, 'id="STsameAsBTjs" data-dynamic-update=1') . '<br />';
 } else if (!empty($this->cart->lists['shipTo'])) {
@@ -71,17 +69,14 @@ if (empty($this->cart->STsameAsBT) and !empty($this->cart->ST) and !empty($this-
 
     if ($item['name'] == 'shipto_address_type_name') continue;
     if (!empty($item['value'])) {
+      echo '<div class="field">';
       echo '<span class="titles">' . $item['title'] . '</span>';
-      if ($item['name'] == 'first_name' || $item['name'] == 'middle_name' || $item['name'] == 'zip') {
-        echo '<span class="values<-' . $item['name'] . '">' . $item['value'] . '</span>';
-      } else {
-        echo '<span class="values">' . $item['value'] . '</span>';
-      }
+      echo '<span class="values">' . $item['value'] . '</span>';
+      echo  '</div>';
     }
   }
   echo '</div>';
 }
-echo '</div>';
 if (!isset($this->cart->lists['current_id'])) {
   $this->cart->lists['current_id'] = 0;
 }
